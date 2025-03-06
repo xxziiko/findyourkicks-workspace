bb73392da819829f58fd74cca3533fdfc17c2a62
xxziiko <xxziiko@gmail.com>
2025-03-06 18:35:47 +0900

    chore: 콘솔 주석처리

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -7,7 +7,8 @@ export async function GET(request: Request) {
   const code = searchParams.get('code');
   // if "next" is in param, use it as the redirect URL
   const next = searchParams.get('next') ?? '/';
-  console.log('Redirecting to:', next);
+  // console.log('Redirecting to:', next);
+
   const supabase = await createClient();
 
   if (code) {
