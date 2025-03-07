859077e0764e8d13422779f725f1cd3391ac26e5
xxziiko <xxziiko@gmail.com>
2025-03-08 01:25:38 +0900

    remove: import 제거

--- src/app/api/auth/kakao/route.ts
+++ src/app/api/auth/kakao/route.ts
@@ -1,5 +1,4 @@
 import { createClient } from '@/app/lib/utils/supabase/server';
-import { redirect } from 'next/dist/server/api-utils';
 import { NextResponse } from 'next/server';
 
 export async function GET(request: Request) {
