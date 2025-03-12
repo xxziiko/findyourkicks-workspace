f3cc1fd1378e60b0866badb4c80588331c615537
xxziiko <xxziiko@gmail.com>
2025-03-12 22:46:17 +0900

    style: Loading UI

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,10 @@
+import { Loader } from 'lucide-react';
+import styles from './loading.module.scss';
+
 export default function Loading() {
-  return <div>로딩!</div>;
+  return (
+    <div className={styles.background}>
+      <Loader color="#fff" />
+    </div>
+  );
 }
