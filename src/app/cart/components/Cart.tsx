bf1013c5f49e273cfcde861a02570358b82bcd99
xxziiko <xxziiko@gmail.com>
2025-03-14 23:50:41 +0900

    fix: CartView 서버 렌더링 비활성화(hydration)

--- src/app/cart/components/Cart.tsx
+++ src/app/cart/components/Cart.tsx
@@ -1,8 +1,10 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { useCart } from '../hooks';
-import { CartView } from '../ui/index';
 import type { CartListProps } from './CartList';
+
+const CartView = dynamic(() => import('../ui/CartView'), { ssr: false });
 
 export interface CartViewProps extends CartListProps {
   totalProduct: number;
