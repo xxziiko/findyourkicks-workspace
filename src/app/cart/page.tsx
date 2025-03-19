cc3a488051f61083e4df79d115cfc6def8fb3640
xxziiko <xxziiko@gmail.com>
2025-03-19 20:17:13 +0900

    style: 장바구니 페이지loading Skeleton UI

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,11 +1,5 @@
-import { Suspense } from 'react';
-import Loading from '../loading';
 import { Cart } from './_features';
 
 export default function CartPage() {
-  return (
-    <Suspense fallback={<Loading />}>
-      <Cart />
-    </Suspense>
-  );
+  return <Cart />;
 }
