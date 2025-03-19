b4d55429d6a03631d9a2dcc8fc9034edc3a390b5
xxziiko <xxziiko@gmail.com>
2025-03-19 19:55:55 +0900

    style: 상품리스트 loading skeleton UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
 import { fetchNaverData } from '@/lib/api';
-import { Suspense } from 'react';
-import Loading from './loading';
 import ProductList from './product/_features/ProductList';
 
 const fetchForSSG = async () => await fetchNaverData();
@@ -8,9 +6,5 @@ const fetchForSSG = async () => await fetchNaverData();
 export default async function Home() {
   const initialProducts = await fetchForSSG();
 
-  return (
-    <Suspense fallback={<Loading />}>
-      <ProductList initialProducts={initialProducts} />
-    </Suspense>
-  );
+  return <ProductList initialProducts={initialProducts} />;
 }
