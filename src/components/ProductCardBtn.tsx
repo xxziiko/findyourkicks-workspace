bb32ed766c3c0b627463e4aa48c124a0fa53ce0a
xxziiko <xxziiko@gmail.com>
2025-03-21 01:59:05 +0900

    style: product list 폰트 사이즈, 레이아웃 수정

--- src/components/ProductCardBtn.tsx
+++ src/components/ProductCardBtn.tsx
@@ -15,19 +15,13 @@ const Card = ({ title, src, brand, price, onClick }: Card) => {
     <button type="button" className={styles.card} onClick={onClick}>
       <div className={styles.card__content}>
         <Image src={src} alt="product" width="13rem" height="13rem" />
-
         <div className={styles.card__details}>
           <p className={styles['card__details--brand']}>{brand}</p>
           <p>{title.replace(/(<b>|<\/b>)/g, '')}</p>
         </div>
       </div>
 
-      <div>
-        <p className={styles.card__price}>
-          {Number(price).toLocaleString()} 원
-        </p>
-        <p className={styles.card__sub}>비회원가</p>
-      </div>
+      <p className={styles.card__price}>{Number(price).toLocaleString()} 원</p>
     </button>
   );
 };
