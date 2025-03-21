2ffca68aaab70cc56d8e7dd03202535f85ae2dce
xxziiko <xxziiko@gmail.com>
2025-03-21 14:48:36 +0900

    style: 레이아웃 컴포넌트 추가(CardLayout)

---
+++ src/components/layouts/CardLayout.tsx
@@ -0,0 +1,28 @@
+import Button from '../Button';
+import styles from './CardLayout.module.scss';
+
+export default function CardLayout({
+  children,
+  title,
+  label = false,
+  type,
+}: {
+  children: React.ReactNode;
+  title?: string;
+  label?: boolean;
+  type?: string;
+}) {
+  return (
+    <div className={styles[`card_${type}`] ?? styles.card}>
+      <div className={styles.card__title}>
+        <h4>{title}</h4>
+        {label && <Label text="주소 변경" />}
+      </div>
+      {children}
+    </div>
+  );
+}
+
+function Label({ text }: { text: string }) {
+  return <Button text={text} onClick={() => {}} variant="label" />;
+}
