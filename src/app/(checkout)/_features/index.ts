4661ea4609d277b0e6b83b71f885e557d341a168
xxziiko <xxziiko@gmail.com>
2025-03-22 17:40:38 +0900

    feat: [#14]ProductInfo component UI

--- src/app/(checkout)/_features/index.ts
+++ src/app/(checkout)/_features/index.ts
@@ -1,3 +1,4 @@
 export { default as OrderCard } from './OrderCard';
 export { default as NoListData } from './NoListData';
 export { default as CartList } from './CartList';
+export { default as ProductInfo } from './ProductInfo';
