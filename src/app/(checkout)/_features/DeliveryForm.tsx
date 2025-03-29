aef4a7c16dde5d4b9fa628eb1b5da584ea9c4ed6
xxziiko <xxziiko@gmail.com>
2025-03-29 22:31:54 +0900

    refactor: layout, 리스트 위치 변경

--- src/app/(checkout)/_features/DeliveryForm.tsx
+++ src/app/(checkout)/_features/DeliveryForm.tsx
@@ -3,11 +3,11 @@ import { Button } from '@/components';
 import styles from './DeliveryForm.module.scss';
 import useDeliveryForm from './useDeliveryForm';
 
-const FORM_TITLE = ['배송지 명', '주문자 명', '연락처'];
+const FORM_TITLE = ['주문자 명', '연락처', '배송지 명'];
 const FORM_PLACEHOLDER = [
-  '배송받을 주소의 이름을 입력해주세요.',
   '받으실 분의 이름을 입력해주세요.',
   ' - 없이 휴대폰 번호를 입력해주세요.',
+  '배송받을 주소의 이름을 입력해주세요.',
 ];
 
 declare global {
@@ -25,10 +25,7 @@ export default function DeliveryForm() {
         {FORM_TITLE.map((title, i) => (
           <div className={styles.form__inner} key={title}>
             <p>{title}</p>
-            <input
-              className={styles.form__input}
-              placeholder={FORM_PLACEHOLDER[i]}
-            />
+            <input placeholder={FORM_PLACEHOLDER[i]} />
           </div>
         ))}
       </div>
@@ -36,11 +33,7 @@ export default function DeliveryForm() {
       <div className={styles.form__wrapper}>
         <h5>주소 검색</h5>
         <div className={styles.form__address}>
-          <input
-            className={styles.form__input}
-            value={address.zonecode}
-            readOnly
-          />
+          <input value={address.zonecode} readOnly />
           <Button
             text="우편번호 찾기"
             variant="lined--r"
@@ -48,16 +41,10 @@ export default function DeliveryForm() {
           />
         </div>
 
-        <input
-          className={styles.form__input}
-          value={address.roadAddress}
-          placeholder="우편번호"
-          readOnly
-        />
+        <input value={address.roadAddress} placeholder="우편번호" readOnly />
 
         <div className={styles.form__address}>
           <input
-            className={styles.form__input}
             value={address.extraAddress}
             placeholder="도로명주소"
             readOnly
