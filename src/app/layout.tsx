659fb2a2c6c4006627e46ffbffa471d15a4eed7f
xxziiko <xxziiko@gmail.com>
2025-04-19 02:14:30 +0900

    chore: 공백 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import type { Metadata } from 'next';
 import Image from 'next/image';
 import styles from './layout.module.scss';
 import { Providers } from './providers';
+
 export const metadata: Metadata = {
   title: 'SHOP | findyourkicks',
   description: 'find your kicks',
