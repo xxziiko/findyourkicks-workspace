937252faf52b4e344755a530cbb232803a68b03d
xxziiko <xxziiko@gmail.com>
2025-04-24 00:09:53 +0900

    refactor: 컴포넌트(CheckoutSteps) 로직 개선 및 경로 상수 추가

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -3,7 +3,6 @@ export { default as CartBadge } from './CartBadge';
 export { default as Image } from './Image';
 export { default as QuantityController } from './QuantityController';
 export { default as CheckBox } from './checkbox/CheckBox';
-export { default as CheckoutSteps } from './CheckoutSteps';
 export { default as Dropdown } from './dropdown/Dropdown';
 export { default as NoData } from './NoData';
 export { default as CartNavLink } from './cart-badge/CartNavLink';
