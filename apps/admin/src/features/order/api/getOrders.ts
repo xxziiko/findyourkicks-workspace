417b715cd3a75073d3743c442a71ce3c888145e8
tomatopickles404 <xxziiko@gmail.com>
2025-08-20 21:03:41 +0900

    chore(admin): 불필요한 주석 삭제

--- apps/admin/src/features/order/api/getOrders.ts
+++ apps/admin/src/features/order/api/getOrders.ts
@@ -11,7 +11,7 @@ const getOrdersSchema = z.object({});
 type OrderItem = z.infer<typeof getOrdersSchema>;
 type OrderStatus = keyof typeof statusMap;
 
-// TODO:
+
 const getOrders = async () => {
   const query = supabase.from('orders_view').select('*').then(handleError);
 
